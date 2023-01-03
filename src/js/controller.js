795c69a9f2c28cee6e04828a01a459f8aece559d
wlpeng00 <venomouswing88@gmail.com>
2023-01-03 18:19:22 -0500

    modified:   src/js/controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -143,7 +143,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
 };
 
 init();
