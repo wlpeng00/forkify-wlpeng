794df3ad2347eba9e5e264c481097262b25409d8
wlpeng00 <venomouswing88@gmail.com>
2023-01-03 18:25:06 -0500

    Testing Push

--- src/js/controller.js
+++ src/js/controller.js
@@ -143,6 +143,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('Testing push');
 };
 
 init();
